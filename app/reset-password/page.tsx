a042aefd7d07be1aad259a8fe9eedf41c78a2e9c
kiran96joshi <40299885+kiran96joshi@users.noreply.github.com>
2025-04-29 19:12:58 +0100

    tests

--- app/reset-password/page.tsx
+++ app/reset-password/page.tsx
@@ -1,10 +1,9 @@
 // app/reset-password/page.tsx
+import ResetPasswordForm from './ResetPasswordForm'
+
 export const metadata = {
   title: 'Reset Password',
-  description: 'Choose a new password',
 }
-
-import ResetPasswordForm from './ResetPasswordForm'
 
 export default function ResetPasswordPage() {
   return <ResetPasswordForm />
